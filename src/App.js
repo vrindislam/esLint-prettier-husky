393ffb605bda1f2b0a0cb55bc4ec273629e42d46
Vrindavan Islamov <vrindavanislamov@gmail.com>
2021-12-23 18:31:54 +0200

    added husky and stylints configs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
+import './App.scss';
 
 const App = () => {
-  console.log('LOG');
-  return (
-    <div>
-      <div>Clean code? - never heard about it...</div>
-    </div>
-  );
+  // console.log('LOG');
+  return <div className='app'>Clean code? - never heard about it...</div>;
 };
 
 export default App;
