d4075f211b710d5aa327fca38389121e1b1ade22
Vrindavan Islamov <vrindavanislamov@gmail.com>
2021-12-23 18:34:59 +0200

    husky checking out

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 
 const App = () => {
-  // console.log('LOG');
+  console.log('LOG');
   return <div className='app'>Clean code? - never heard about it...</div>;
 };
 
